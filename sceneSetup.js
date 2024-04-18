9504e34d073eab3ec781d32740c0e8cd8c53d498
Ahmed Jabai <ahmed54.aj@gmail.com>
2024-04-17 21:47:42 -0400

    added buttons to raycaster objects

--- sceneSetup.js
+++ sceneSetup.js
@@ -6,8 +6,6 @@ async function getBosses(bosses) {
     setBossCard(bosses);
 }
 getBosses(bosses);
-
-
 function setBossCard(bosses) {
     // card image
     const floatingBossCard = document.getElementById('bossCard');
@@ -16,15 +14,15 @@ function setBossCard(bosses) {
     const bossText = document.getElementById('bossText');
     bossText.setAttribute('text', `value: ${bosses[0].name}; color: #000`);
 }
-
 // Select the left and right Oculus Touch controllers
 const leftController = document.querySelector('[oculus-touch-controls="hand: left"]');
+const leftRaycaster = leftController.components.raycaster;
 const rightController = document.querySelector('[oculus-touch-controls="hand: right"]');
+const rightRaycaster = rightController.components.raycaster;
 rightController.setAttribute('raycaster', 'objects: .buttons');
 leftController.setAttribute('raycaster', 'objects: .buttons');
 // Add event listener for triggerdown on the left controller
 leftController.addEventListener('triggerdown', function() {
-    const raycaster = leftController.components.raycaster;
     const bossText = document.getElementById('bossText');
     if (raycaster.intersectedEls.length > 0) {
         bossText.setAttribute('text', `value: button is clicked; color: #000`);
@@ -35,7 +33,6 @@ leftController.addEventListener('triggerdown', function() {
 
 // Add event listener for triggerdown on the right controller
 rightController.addEventListener('triggerdown', function() {
-    const raycaster = rightController.components.raycaster;
     const bossText = document.getElementById('bossText');
     if (raycaster.intersectedEls.length > 0) {
         bossText.setAttribute('text', `value: button is clicked; color: #000`);
