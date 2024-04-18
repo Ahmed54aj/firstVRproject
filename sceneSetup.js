6384403866f5ab8c09d1de1ece2f5d2c04d5840f
Ahmed Jabai <ahmed54.aj@gmail.com>
2024-04-17 22:11:11 -0400

    new

--- sceneSetup.js
+++ sceneSetup.js
@@ -15,29 +15,29 @@ function setBossCard(bosses) {
     bossText.setAttribute('text', `value: ${bosses[0].name}; color: #000`);
 }
 // Select the left and right Oculus Touch controllers
-const leftController = document.querySelector('[oculus-touch-controls="hand: left"]');
-const rightController = document.querySelector('[oculus-touch-controls="hand: right"]');
-// Add event listener for triggerdown on the left controller
-leftController.addEventListener('triggerdown', function() {
-    const bossText = document.getElementById('bossText');
-        bossText.setAttribute('text', `value: button is clicked; color: #000`);
-        bossText.setAttribute('text', `value: button not clicked; color: #000`);
-});
+// const leftController = document.querySelector('[oculus-touch-controls="hand: left"]');
+// const rightController = document.querySelector('[oculus-touch-controls="hand: right"]');
+// // Add event listener for triggerdown on the left controller
+// leftController.addEventListener('triggerdown', function() {
+//     const bossText = document.getElementById('bossText');
+//         bossText.setAttribute('text', `value: button is clicked; color: #000`);
+//         bossText.setAttribute('text', `value: button not clicked; color: #000`);
+// });
 
-// Add event listener for triggerdown on the right controller
-rightController.addEventListener('triggerdown', function() {
-    const bossText = document.getElementById('bossText');
-        bossText.setAttribute('text', `value: button is clicked; color: #000`);
-});
+// // Add event listener for triggerdown on the right controller
+// rightController.addEventListener('triggerdown', function() {
+//     const bossText = document.getElementById('bossText');
+//         bossText.setAttribute('text', `value: button is clicked; color: #000`);
+// });
 
-// Add event listener for gripdown on the left controller
-leftController.addEventListener('gripdown', function() {
-    const bossText = document.getElementById('bossText');
-    bossText.setAttribute('text', `value: left gripdown; color: #000`);
-});
+// // Add event listener for gripdown on the left controller
+// leftController.addEventListener('gripdown', function() {
+//     const bossText = document.getElementById('bossText');
+//     bossText.setAttribute('text', `value: left gripdown; color: #000`);
+// });
 
-// Add event listener for gripdown on the right controller
-rightController.addEventListener('gripdown', function() {
-    const bossText = document.getElementById('bossText');
-    bossText.setAttribute('text', `value: right gripdown; color: #000`);
-});
+// // Add event listener for gripdown on the right controller
+// rightController.addEventListener('gripdown', function() {
+//     const bossText = document.getElementById('bossText');
+//     bossText.setAttribute('text', `value: right gripdown; color: #000`);
+// });
