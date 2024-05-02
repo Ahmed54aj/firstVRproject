dd6f58ac5f4cfb29ae05dbf6a2e79dd840404ad2
Ahmed Jabai <ahmed54.aj@gmail.com>
2024-05-01 22:29:08 -0400

    commented out iffy code

--- sceneSetup.js
+++ sceneSetup.js
@@ -16,16 +16,16 @@ function setBossCard(bosses) {
 }
 
 
-const hand = document.getElementById("left-hand");
+const leftHand = document.getElementById("left-hand");
 const fireball = document.getElementById("fireball");
-    hand.addEventListener('triggerdown', function (event) {
+    leftHand.addEventListener('triggerdown', function (event) {
     // fireball.position.set(event.target.position);
      fireball.setAttribute("scale", "1 1 1");
-     fireball.emit("start-animation");
+    //  fireball.emit("start-animation");
      bossText.setAttribute('text', `value: left trigger down; color: #000`);
     });
-    hand.addEventListener('triggerup', function (event) {
-     fireball.emit("stop-animation");
+    leftHand.addEventListener('triggerup', function (event) {
+    //  fireball.emit("stop-animation");
      fireball.setAttribute("scale", "0 0 0");
      bossText.setAttribute('text', `value: left trigger up; color: #000`);
     });
