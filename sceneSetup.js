ebe7694c4541aeadadb49bd3030d37068e49e572
Ahmed Jabai <ahmed54.aj@gmail.com>
2024-05-01 22:10:04 -0400

    removed event handlers being set from a function

--- sceneSetup.js
+++ sceneSetup.js
@@ -13,28 +13,19 @@ function setBossCard(bosses) {
     //  text
     const bossText = document.getElementById('bossText');
     bossText.setAttribute('text', `value: ${bosses[0].name}; color: #000`);
-    setupHandEvents();
 }
 
 
-
-
- function setupHandEvents() {
-    const hand = document.getElementById("left-hand");
-    const fireball = document.getElementById("fireball");
+const hand = document.getElementById("left-hand");
+const fireball = document.getElementById("fireball");
     hand.addEventListener('pinchstarted', function (event) {
     fireball.position.set(event.target.position);
      fireball.setAttribute("scale", "1 1 1");
      fireball.emit("start-animation");
      bossText.setAttribute('text', `value: left hand pinch; color: #000`);
     });
-    
     hand.addEventListener('pinchstarted', function (event) {
      fireball.emit("stop-animation");
      fireball.setAttribute("scale", "0 0 0");
      bossText.setAttribute('text', `value: left hand let go; color: #000`);
     });
- }  
-   
-
-
